3b6c4a27a9f2bb2fb8a7ee5383feefaa21afec1c
tientv0803 <tientv0803@gmail.com>
2018-06-29 10:48:02 +0700

    update file .DAO and remove id place model
    - optimize MongoUltils.java
    - remove id file in place model
    -  edit LinkDAO
    - edit PlaceDAO

--- CrawlerAFID/src/main/java/Model/Comment.java
+++ CrawlerAFID/src/main/java/Model/Comment.java
@@ -3,40 +3,40 @@ package Model;
 import java.util.Date;
 
 public class Comment {
-	private String userName;
-	private String commentDesciption;
-	private String createdDate;
+    private String userName;
+    private String commentDesciption;
+    private String createdDate;
 
-	public Comment() {
-	}
+    public Comment() {
+    }
 
-	public Comment(String userName, String commentDesciption, String createdDate) {
-		this.userName = userName;
-		this.commentDesciption = commentDesciption;
-		this.createdDate = createdDate;
-	}
+    public Comment(String userName, String commentDesciption, String createdDate) {
+        this.userName = userName;
+        this.commentDesciption = commentDesciption;
+        this.createdDate = createdDate;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getCommentDesciption() {
-		return commentDesciption;
-	}
+    public String getCommentDesciption() {
+        return commentDesciption;
+    }
 
-	public void setCommentDesciption(String commentDesciption) {
-		this.commentDesciption = commentDesciption;
-	}
+    public void setCommentDesciption(String commentDesciption) {
+        this.commentDesciption = commentDesciption;
+    }
 
-	public String getCreatedDate() {
-		return createdDate;
-	}
+    public String getCreatedDate() {
+        return createdDate;
+    }
 
-	public void setCreatedDate(String createdDate) {
-		this.createdDate = createdDate;
-	}
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
 }
